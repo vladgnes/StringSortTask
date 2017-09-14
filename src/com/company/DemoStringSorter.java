084b8e3867ca2ae6e96852ea2c335c29b2af8c87
SYNAPSE\vladyslav.hnes <Vhnes02@gmail.com>
2017-09-14 15:30:12 +0300

    DemoStringSorter class was created.

---
+++ src/com/company/DemoStringSorter.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class DemoStringSorter {
+
+    public static void main(String[] args) {
+        StringSorter stringSorter = new StringSorter();
+        stringSorter.sortFile("resources\\lines", 5);
+    }
+}
