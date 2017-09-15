ce2e1d7470f96a9bf2577cefd1e7dbf0d2e2a4a0
SYNAPSE\vladyslav.hnes <Vhnes02@gmail.com>
2017-09-15 11:19:48 +0300

    OpenReader method was added.

--- src/com/company/FileBean.java
+++ src/com/company/FileBean.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class FileBean {
 
@@ -26,5 +28,9 @@ public class FileBean {
 
     public void setFile(File file) {
         this.file = file;
+    }
+
+    public void openReader() throws FileNotFoundException {
+        this.setReader(new BufferedReader(new FileReader(this.file)));
     }
 }
